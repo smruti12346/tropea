dd84bdadbf5e05bf2076d3f2e53edec20ae30fdb
smruti12346 <106754687+smruti12346@users.noreply.github.com>
2023-06-20 17:06:21 +0530

    Delete build directory

--- build/themes/common/javascripts/smb/jquery.contact-number-wrappere556.js
+++
@@ -1,8 +0,0 @@
-// wrap location phone and fax number plugin
-;(function($) {
-	$.fn.contactNumberWrapper = function() {
-		this.each(function() {
-			console.log('contactNumberWrapper plugin is not needed anymore. Please remove it from theme');
-		});
-	};
-})(jQuery);
