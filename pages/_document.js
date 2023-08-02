2211da2bb192b65f989caad0fc3977d49d863404
smruti12346 <smruti.thetechnovate@gmail.com>
2023-08-02 17:14:07 +0530

    second aug

--- pages/_document.js
+++ pages/_document.js
@@ -1,6 +1,4 @@
 import { Html, Head, Main, NextScript } from "next/document";
-import Script from "next/script";
-
 export default function Document() {
   return (
     <Html lang="en">
@@ -9,20 +7,23 @@ export default function Document() {
           name="google-site-verification"
           content="N7pIb57UraO1g7r-51DdTPsCQJSXxTlnOkGFcWx8Rfg"
         />
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+        />
         <script
           async
           src="https://www.googletagmanager.com/gtag/js?id=G-W0S5M63CZK"
         ></script>
         <script
           dangerouslySetInnerHTML={{
-            __html: `(window.dataLayer = window.dataLayer || []; function gtag(){" "}
-          {dataLayer.push(arguments)}
-          gtag("js", new Date()); gtag("config", "G-W0S5M63CZK"))`,
+            __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-W0S5M63CZK');
+          `,
           }}
-        />
-        <link
-          rel="stylesheet"
-          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
         />
       </Head>
       <body>
