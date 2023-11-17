18be93a7abd4c8af4a8ea7d6d31da6a0971cd0c3
smruti12346 <smruti.thetechnovate@gmail.com>
2023-11-17 19:16:34 +0530

    17th commit

--- Auth.js
+++ Auth.js
@@ -1,4 +1,5 @@
-const api_url = "https://api.tonytropeadc.com/wp-json/wp/v2";
+//const api_url = "https://api.tonytropeadc.com/wp-json/wp/v2";
+const api_url = "https://tonytropeadc.com/blog/wp-json/wp/v2";
 // const url = "http://162.241.116.186/tropea";
 const url = "https://tonytropeadc.com";
 export { api_url, url };
--- out/_next/static/fVrLD_1S-jIHjL4BlvyYf/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002F[slug]","\u002Fblog","\u002Fblog\u002F[single]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
